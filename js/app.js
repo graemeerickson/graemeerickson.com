31859423b7dc0403d56da2960d06ab8ae4115bc3
graemeerickson <graemeerickson@gmail.com>
2018-08-23 11:44:53 -0700

    Implemented scrollreveal

---
+++ js/app.js
@@ -0,0 +1,7 @@
+console.log('hello world');
+
+ScrollReveal().reveal('.top-nav');
+ScrollReveal().reveal('.opening-section');
+ScrollReveal().reveal('.section-title');
+ScrollReveal().reveal('.skills');
+ScrollReveal().reveal('.project');
