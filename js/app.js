bac2729ef15756047f6cea46acb62d8782d00bab
graemeerickson <graemeerickson@gmail.com>
2018-08-27 21:38:50 -0700

    Tweaked scrollreveal

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
-ScrollReveal().reveal('.top-nav');
-ScrollReveal().reveal('.opening-section');
-ScrollReveal().reveal('.section-title');
-ScrollReveal().reveal('.skills');
-ScrollReveal().reveal('.project');
+let sr = ScrollReveal({
+    reset: false,
+    duration: 300,
+    easing: 'cubic-bezier(.694,0,.335,1)',
+    scale: 1,
+    viewFactor: 0.3,
+  });
+
+sr.reveal('.opening-section');
+sr.reveal('.section-title');
+sr.reveal('.skills');
+sr.reveal('.project');
