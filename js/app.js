2fd5b3474cfd0857fa31508b28989619ac47d89b
graemeerickson <graemeerickson@gmail.com>
2018-09-19 10:12:11 -0700

    Replaced scrollReveal with new library for scroll animations

--- js/app.js
+++ js/app.js
@@ -18,15 +18,5 @@ $(document).ready(function () {
     }, 800);
   });
 
-  let sr = ScrollReveal({
-    reset: false,
-    duration: 300,
-    easing: 'cubic-bezier(.694,0,.335,1)',
-    scale: 1,
-    viewFactor: 0.3,
-  });
-
-  sr.reveal('.opening-section');
-  sr.reveal('.skills');
-  sr.reveal('.project');
+  AOS.init(); // scroll animations
 });
